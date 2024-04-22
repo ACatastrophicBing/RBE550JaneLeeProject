d1b60651ac2c4060218b19f5cb6c9e7e7d8615ac
Chandler Garcia <57022847+ACatastrophicBing@users.noreply.github.com>
2024-04-22 12:57:03 -0400

    Humans added, robot map updates, cspace generated, frontier space created
    The lidar search radius is not optimal, there is a way to track all vertices and then if a vertex is close enough to get the parent and generate the lines, but I can't be bothered to optimize anymore at this point and it runs really quick if the lidar range is shrunk a lot, or if we just don't use the lidar

--- Examples/example.py
+++ Examples/example.py
@@ -12,33 +12,67 @@ from RobotSim373 import *
 from PRM import PRM
 import math
 import skimage as ski
+from skimage.morphology import isotropic_dilation
 
 
 class Map:
-    def __init__(self, env, robot, boxes,definition=[100,100], lidar_range=0,wrld_size=[24,24], map_update_rate = 10):
+    def __init__(self, env, robot, boxes, humans=[],definition=[100,100], lidar_range=10.0,wrld_size=[24,24],
+                 map_update_rate = 100, global_map_init = True, c_space_dilation = 1.0, human_radius = 0.5,
+                 use_global_knowledge = False):
         self.env = env               # The environment we are in
-        self.robot = robot         # How many actors we have in the environment that are mapping
+        self.robot = robot           # How many actors we have in the environment that are mapping
+        self.humans = humans         # How many random movement agents we have in the environment
+        self.human_size = human_radius
         self.definition = definition # How detailed of a map we want
-        self.lidar_range = lidar_range
+        self.definition_conversion = [definition[0]/wrld_size[0], definition[1]/wrld_size[1]]
+        self.lidar_range = lidar_range * int(self.definition_conversion[0])
         self.wrld_size = wrld_size
-        self.map = np.full((definition[0], definition[1]),1)
         self.boxes = [box.body for box in boxes]
-        self.PRM = PRM(self.map)
         self.cell_map = []
+        self.human_size_map = []
+        self.global_map_init = global_map_init
+        self.c_space_dilation = c_space_dilation
+        self.use_global_knowledge = use_global_knowledge
 
         self.robot_position = self.world_to_map(self.robot['center'].position)
-        self.create_cell_map()
+        self.create_cell_maps()
         self.robot_flag = False # If the robot updates its position in world, flag this
         self.map_flag = False   # If the map has any changes compared to what it previously was, flag this
         self.map_update_rate = map_update_rate # How often we check the map to update things, map will also update if robot moves
 
+        self.box_angles = []
+        self.box_positions = []
+        self.box_vertices = []
+        for box in self.boxes:
+            self.box_angles.append(box.angle)
+            self.box_positions.append(self.world_to_map(box.position))
+            self.box_vertices.append(box.fixtures[0].shape.vertices)
+
+        self.human_positions = []
+        for human in self.humans:
+            self.human_positions.append(self.world_to_map(human.position))
+
+        self.map = np.zeros((definition[0], definition[1]), dtype=bool) # 0 is empty 1 is blocked
+
+        self.frontier_map = np.full(self.map.shape, 1, dtype=bool) # Is this efficient? No, but this'll be needed later
+
         self.update_global_map()
-        self.robot_map = self.map.copy()
+        if global_map_init:
+            self.robot_map = self.map.copy()
+        else:
+            self.robot_map = np.full(self.map.shape, 1, dtype=bool)
+
+        self.last_map_update = 0
+
+        self.robot_cspace = np.zeros(self.map.shape, dtype=bool)
 
     def update(self, tick):
         # for robot in self.robots:
         #     continue
-        self.update_global_map()
+
+        if int(tick * 1000) - self.last_map_update > self.map_update_rate:
+            self.update_global_map()
+            self.last_map_update = tick
 
         perceived_position = self.world_to_map(self.robot['center'].position)
         if perceived_position != self.robot_position:
@@ -47,27 +81,51 @@ class Map:
         else:
             self.robot_flag = False
         if tick % self.map_update_rate == 0 or self.robot_flag:
-            for pos in self.cell_map:
-                x = pos[0] + self.robot_position[0]
-                y = pos[1] + self.robot_position[1]
-                if 0 <= x < self.definition[0] and 0 <= y < self.definition[1]:
-                    self.robot_map[x][y] = self.map[x][y]
+            if self.use_global_knowledge:
+                self.robot_map = self.map
+            else:
+                for pos in self.cell_map:
+                    x = pos[0] + self.robot_position[0]
+                    y = pos[1] + self.robot_position[1]
+                    if 0 <= x < self.definition[0] and 0 <= y < self.definition[1]:
+                        self.robot_map[x][y] = self.map[x][y]
 
 
     def update_global_map(self):
-        for box in self.boxes:
-            angle = box.angle
-            position = self.world_to_map(box.position)
-            vertices = len(box.fixtures[0].shape.vertices)
-            for vert in range(vertices):
-                self.generate_obstacle_lines(position,
-                             self.world_to_map(self.rotate(box.fixtures[0].shape.vertices[vert%vertices], angle)),
-                             self.world_to_map(self.rotate(box.fixtures[0].shape.vertices[(vert+1)%vertices], angle)))
-            self.fill_inside_box(position)
+        for i in range(len(self.boxes)):
+            if (self.box_angles[i] != self.boxes[i].angle or
+                    self.box_positions[i] != self.world_to_map(self.boxes[i].position)):
+                verts = len(self.box_vertices[i])
+                for vert in range(verts):
+                    self.remove_obstacle_lines(self.box_positions[i],
+                                self.world_to_map(self.rotate(self.box_vertices[i][vert%verts], self.box_angles[i])),
+                                self.world_to_map(self.rotate(self.box_vertices[i][(vert+1)%verts], self.box_angles[i])))
+                self.box_angles[i] = self.boxes[i].angle
+                self.box_positions[i] = self.world_to_map(self.boxes[i].position)
+                for vert in range(verts):
+                    self.generate_obstacle_lines(self.box_positions[i],
+                                self.world_to_map(self.rotate(self.box_vertices[i][vert % verts],self.box_angles[i])),
+                                self.world_to_map(self.rotate(self.box_vertices[i][(vert + 1) % verts],self.box_angles[i])))
+
+        for i in range(len(self.humans)): # These dudes move every time step so you have to update the global map
+            if self.human_positions[i] != self.world_to_map(self.humans[i].position):
+                for pos in self.human_size_map:
+                    x = pos[0] + self.human_positions[i][0]
+                    y = pos[1] + self.human_positions[i][1]
+                    if 0 <= x < self.definition[0] and 0 <= y < self.definition[1]:
+                        self.map[x][y] = 0
+                self.human_positions[i] = self.world_to_map(self.humans[i].position)
+                for pos in self.human_size_map:
+                    x = pos[0] + self.human_positions[i][0]
+                    y = pos[1] + self.human_positions[i][1]
+                    if 0 <= x < self.definition[0] and 0 <= y < self.definition[1]:
+                        self.map[x][y] = 1
+
+
 
 
     def world_to_map(self,p1):
-        return (math.floor(p1[0]/self.wrld_size[0] * self.definition[0]), math.floor(p1[1]/self.wrld_size[1] * self.definition[1]))
+        return (math.floor(p1[0] * self.definition_conversion[0]), math.floor(p1[1] * self.definition_conversion[1]))
 
     def rotate(self, vect, angle):
         (x, y) = vect
@@ -75,30 +133,38 @@ class Map:
         newy = x * math.sin(angle) + y * math.cos(angle)
         return [newx, newy]
 
-    def fill_inside_box(self,p):
+    def flood_fill(self,p):
         x, y = p
-        if self.map[x][y] != 1:  # If the point is not inside the box,
+        if self.map[x][y] != 0:  # If the point is not inside the box,
             return
-        self.map[x][y] = 0  # Fill the point
+        self.map[x][y] = 1  # Fill the point
         # Check and fill neighboring points
         for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             new_x, new_y = x + dx, y + dy
             if 0 <= new_x < self.definition[0] and 0 <= new_y < self.definition[1]:
-                self.fill_inside_box([new_x, new_y])
+                self.flood_fill([new_x, new_y])
 
-    def generate_obstacle_lines(self,position, p1,p2):
+    def generate_obstacle_lines(self, position, p1,p2):
+        line = ski.draw.line(p1[0] + position[0], p1[1] + position[1], p2[0] + position[0], p2[1] + position[1])
+        for i in range(len(line[:][0])):
+            self.map[line[0][i]][line[1][i]] = 1
+
+    def remove_obstacle_lines(self, position, p1,p2):
         line = ski.draw.line(p1[0] + position[0], p1[1] + position[1], p2[0] + position[0], p2[1] + position[1])
         for i in range(len(line[:][0])):
             self.map[line[0][i]][line[1][i]] = 0
 
-    def visualize(self):
-        pos = self.robot['center'].position
-        pos = self.world_to_map([pos.x,pos.y])
-        self.PRM.sample(n_pts=1000, sampling_method="random")
-        self.PRM.search(pos, self.world_to_map([20,10]))
-        self.PRM.draw_map()
 
-    def create_cell_map(self):
+    def path_plan(self, algorithm):
+        self.robot_cspace = isotropic_dilation(self.robot_map,radius=int(self.c_space_dilation * self.definition_conversion[0]))
+        # self.robot_cspace = self.robot_map
+        if algorithm == "PRM":
+            self.PRM = PRM(self.robot_cspace)
+            self.PRM.sample(n_pts=500, sampling_method="random")
+            self.PRM.search(self.robot_position, self.world_to_map([20, 10]))
+            self.PRM.draw_map()
+
+    def create_cell_maps(self):
         """
         Creates a cell map that stores all the cells within a given radius around 0,0 to update our map.
 
@@ -117,11 +183,23 @@ class Map:
                 if distance <= radius:
                     self.cell_map.append([x,y])
 
+
+        for x in range(-int(self.human_size * self.definition_conversion[0]), int(self.human_size * self.definition_conversion[0])):
+            for y in range(-int(self.human_size * self.definition_conversion[1]),int(self.human_size * self.definition_conversion[1])):
+                distance = math.sqrt((x ** 2) + (y ** 2))
+                if distance <= radius:
+                    self.human_size_map.append([x, y])
+
+
 class Simulator:
-    def __init__(self, env, num_robots, rand_obstacles=0, map_selector=None,wrld_size=[24,24]):
+    def __init__(self, env, num_robots, rand_obstacles=0, map_selector=None,wrld_size=[24,24], num_humans=0):
         self.env = env
         self.robots = []
         self.obstacles = []
+        self.humans = []
+        for i in range(num_humans):
+            self.humans.append(Disk(self.env, x=Simulator.randbetween(8, 22), y=Simulator.randbetween(8, 22),
+                    radius=0.5, angle=Simulator.randbetween(0, 360), color='red'))
 
         if map_selector is not None:
             # TODO : Create maps to allow robots to properly explore environment
@@ -130,6 +208,7 @@ class Simulator:
             for i in range(rand_obstacles):
                 self.obstacles.append(Box(self.env, x=Simulator.randbetween(8, 22), y=Simulator.randbetween(8, 22),
                     width=Simulator.randbetween(.2, 1.2), height=2, angle=Simulator.randbetween(0, 360), color='purple'))
+
 
         # for rob in range(num_robots): # TODO : Modify the environment to handle multiple robots
         #     self.robots.append(Robot(env))
@@ -140,7 +219,7 @@ class Simulator:
         # print(type(self.robots))
         # print(dir(self.robots))
 
-        self.map = Map(env, self.robots, boxes=self.obstacles, definition=[1000,1000], lidar_range=20,wrld_size=wrld_size)
+        self.map = Map(env, self.robots, boxes=self.obstacles, definition=[1000,1000], lidar_range=5,wrld_size=wrld_size,humans=self.humans)
 
     def build(self, robot):
         # TODO : Generate random positions of robots that don't overlap with other robots
@@ -183,6 +262,14 @@ def act(t, robot):
 
     map.update(t)
 
+    for human in sim.humans:
+        if human.read_distance() < 2:
+            # Turn
+            human.F = 0
+            human.τ = 0.01
+        else:
+            human.F = 0.1
+
 
 def robot_controller( robot):
     return [0,0]
@@ -191,9 +278,11 @@ if __name__ == "__main__":
     wrld_size = [24,24]
     env = Environment(wrld_size[0], wrld_size[1])
     num_robots = 1
-    sim = Simulator(env, 1, rand_obstacles=20,wrld_size=wrld_size)
+    num_humans = 2
+    num_obstacles = 20
+    sim = Simulator(env, 1, rand_obstacles=20,wrld_size=wrld_size, num_humans=num_humans)
     map = sim.map
-    run_sim(env, act, figure_width=6, total_time=13, dt_display=10)
-    map.visualize()
+    run_sim(env, act, figure_width=6, total_time=10, dt_display=10)
+    map.path_plan("PRM")
 
 # NOTE : If you are looking for a function call or data, do print(dir(data)) and for type do print(type(data))
